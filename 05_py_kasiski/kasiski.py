b3397b9393d5a737c0c0dd2cd6fc992a1dea1de3
1197-25-HTL3R <1197@htl.rennweg.at>
2025-02-08 18:23:40 +0100

    05_py_kasiski: 2.Commit --> encrypt Methode

--- 05_py_kasiski/kasiski.py
+++ 05_py_kasiski/kasiski.py
@@ -3,4 +3,12 @@ class Caeser :
         ret = plaintext.lower()
         return "".join(i for i in ret if ret.isalpha())
 
+    def encrypt(plaintext:str, key:str = None) -> str:
+        if key is None:
+            return plaintext
+        else:
+            ret = Caeser.to_lowercase_letter_only(plaintext)
+            for i in ret:
+                ret = ret.replace(i,i+key)
+            return ret
 
