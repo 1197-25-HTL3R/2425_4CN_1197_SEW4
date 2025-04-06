9a054715ec1b54e3a50174a803888c67c21523fd
1197-25-HTL3R <1197@htl.rennweg.at>
2025-04-06 22:02:18 +0200

    08_py_comprehension: 2.Commit -> B.2 Unterprogramm mit Docstring fertiggestellt

--- 08_py_comprehension/spellcheck.py
+++ 08_py_comprehension/spellcheck.py
@@ -15,15 +15,30 @@ def read_all_words(filename: str) -> set[str]:
 
     Returns:
         set[str]: Eine Menge aller Wörter aus der Datei.
-
-    Beispiel:
-    >>> #Angenommen, 'de-en.txt' enthält:
-    >>> #Hallo
-    >>> #Welt
-    >>> #Python
-    >>> read_all_words("de-en.txt")
-    {'hallo', 'welt', 'python'}
     """
 
     with open(filename, mode = "r", encoding = "utf-8") as file:
         return {line.strip() for line in file if line.strip()}
+
+
+def split_word(wort:str) -> List[Tuple[str, str]]:
+    """
+        Erzeugt eine Liste von Aufteilungen des Wortes, indem jeweils ein Zeichen
+        ausgelassen wird. Für jedes Zeichen im Wort wird ein Tupel (head, tail) erzeugt,
+        wobei 'head' der Teil vor dem aktuellen Zeichen und 'tail' der Teil nach dem
+        aktuellen Zeichen ist.
+
+        Beachte: Für ein Wort der Länge n werden n Tupel erzeugt.
+
+        Beispiele:
+        >>> split_word("abc")
+        [('', 'abc'), ('a', 'bc'), ('ab', 'c'), ('abc', '')]
+        """
+    ret: List[Tuple[str, str]] = []
+
+    for i in range(len(wort)+1):
+        ret += [(wort[0:i], wort[i:len(wort)])]
+
+    return ret
+
+
